43b59b894431d39b4846b1a5bee9617ab5f4492d
Andrii Pereverziev <y4t6official@gmail.com>
2018-04-05 21:53:30 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import './main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {Router, Route} from 'react-router'
+
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
+import Socials from 'containers/socials'
+
+const store = createStore(reducers, composeWithDevTools(
+	applyMiddleware(thunk)
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={history}>
+			<Route component={Layout}>
+				<Route path='/' component={Phones} />
+				<Route path='/socials' component={Socials} />
+			</Route>
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+)
